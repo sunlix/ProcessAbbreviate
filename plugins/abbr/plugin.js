14ffa18c270bf1799c7239c03c4d8f1090517689
Sven Schüring <ssch@zenmotion.de>
2015-03-06 19:07:08 +0100

    updated command definition for proper allowedContent

--- plugins/abbr/plugin.js
+++ plugins/abbr/plugin.js
@@ -15,12 +15,26 @@ CKEDITOR.plugins.add('abbr', {
     // The plugin initialization logic goes inside this method.
     init: function(editor) {
 
-        editor.addCommand('abbr', new CKEDITOR.dialogCommand('abbrDialog'));
+        // Define an editor command that opens our dialog window.
+        editor.addCommand('abbr', new CKEDITOR.dialogCommand('abbrDialog', {
+            // Allow the abbr tag with an required title attribute.
+            allowedContent: 'abbr[!title,lang]',
+            // Require the abbr tag to be allowed for the feature to work.
+            requiredContent: 'abbr',
+            // Prefer abbr over acronym. Transform acronym elements into abbr elements.
+            contentForms: [
+                'abbr',
+                'acronym'
+            ]
+        }));
+        // Create a toolbar button that executes the above command.
         editor.ui.addButton('Abbr', {
+            // The text part of the button (if available) and the tooltip.
             label: 'Insert Abbreviation',
+            // The command to execute on click.
             command: 'abbr',
+            // The button placement in the toolbar (toolbar group name).
             toolbar: 'insert',
-            allowedContent: 'abbr[title,lang]'
         });
 
         inReadOnly = 0;
@@ -76,9 +90,6 @@ CKEDITOR.plugins.add('abbr', {
             }
         }
 
-        // Register our dialog file. this.path is the plugin folder path.
-        CKEDITOR.dialog.add('abbrDialog', this.path + 'dialogs/abbr.js');
-
         // add contextmenu
         if (editor.contextMenu) {
             editor.addMenuGroup('abbrGroup');
@@ -96,5 +107,8 @@ CKEDITOR.plugins.add('abbr', {
                 }
             });
         }
+
+        // Register our dialog file. this.path is the plugin folder path.
+        CKEDITOR.dialog.add('abbrDialog', this.path + 'dialogs/abbr.js');
     }
 });
